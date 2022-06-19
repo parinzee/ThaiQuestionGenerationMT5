3e8b833d17e35740b9b425a3c51bfaf531fb7696
Parinthapat Pengpun <parinzee@protonmail.com>
2022-06-19 22:50:11 +0700

    Added model parameter settings

--- frontend/src/components/Inference.jsx
+++ frontend/src/components/Inference.jsx
@@ -6,7 +6,7 @@ function Inference() {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className="grid row-span-6 md:col-span-2 h-full">
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-between row-span-6 md:col-span-2 h-full">
             <ModelInput register={register} />
             <Settings register={register} />
         </form>
--- frontend/src/components/Settings.jsx
+++ frontend/src/components/Settings.jsx
@@ -2,6 +2,24 @@ function Settings({ register }) {
     return (
         <div className="window font-sans mt-3 text-black dark:text-white row-span-1">
             <h2 className="text-2xl md:text-3xl text-black dark:text-white font-bold mt-1">Settings</h2>
+            <div class="grid gap-6 mb-6 lg:grid-cols-2">
+                <div>
+                    <label for="num_beams" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">num_beams</label>
+                    <input {...register("num_beams")} type="number" id="num_beams" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="integer" required defaultValue={3}></input>
+                </div>
+                <div>
+                    <label for="repetition_penalty" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">repetition_penalty</label>
+                    <input {...register("repetition_penalty")} type="number" step={0.01} id="repitition_penalty" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="integer" required defaultValue={2.75}></input>
+                </div>
+                <div>
+                    <label for="top_p" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">top_p</label>
+                    <input {...register("top_p")} type="number" id="repitition_penalty" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="integer" required defaultValue={50}></input>
+                </div>
+                <div>
+                    <label for="top_k" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">top_k</label>
+                    <input {...register("top_k")} type="number" id="repitition_penalty" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="integer" required defaultValue={20}></input>
+                </div>
+            </div>
         </div>
     )
 }
