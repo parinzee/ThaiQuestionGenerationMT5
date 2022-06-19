f237950123eb4f9eaf80778c5e9693bdb3308fbe
Parinthapat Pengpun <parinzee@protonmail.com>
2022-06-19 08:57:56 +0700

    Finished up server

--- server/main.py
+++ server/main.py
@@ -1,18 +1,33 @@
+import logging
+from typing import Literal
+
 import torch
+import psutil
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import MT5ForConditionalGeneration, MT5TokenizerFast
-from enum import Enum
 
 
-class Models(Enum):
-    default = "mT5-small-thai-multiple-e2e-qg"
-    separated = "mT5-small-thai-multiple-e2e-qg-sep"
-    number_separated = "mT5-small-thai-multiple-e2e-qg-numsep"
+logging.info("Loading Models")
+models = {
+    "default": {
+        "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg"),
+        "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg")
+    },
+    "separated": {
+        "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg-sep"),
+        "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg-sep")
+    },
+    "number_separated": {
+        "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg-numsep"),
+        "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg-numsep")
+    } 
+}
 
 
 class Args(BaseModel):
-    model: Models = Models.default
+    model: Literal["default", "separated", "number_separated"] = "default"
     num_beams: int = 3
     max_length: int = 1024
     repetition_penalty: float = 2.5
@@ -22,12 +37,17 @@ class Args(BaseModel):
     top_k: int = 20
     num_return_sequences: int = 1
 
+logging.info("Starting App")
 app = FastAPI()
 
+@app.get("/")
+def health_check():
+    return {"models": list(models.keys()), "cpu_percent": psutil.cpu_percent(), "ram_percent": psutil.virtual_memory().percent} 
+
 @app.post("/")
-def root(input_text: str, args: Args):
-    model = MT5ForConditionalGeneration.from_pretrained(f"parinzee/{args.model.value}", return_dict=True)
-    tokenizer = MT5TokenizerFast.from_pretrained(f"parinzee/{args.model.value}")
+def model_endpoint(input_text: str, args: Args):
+    model = models[args.model][model]
+    tokenizer= models[args.model][tokenizer]
 
     with torch.no_grad():
         input_ids = tokenizer.encode(input_text, return_tensors="pt", add_special_tokens=True)
