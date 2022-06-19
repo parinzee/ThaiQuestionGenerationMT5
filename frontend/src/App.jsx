ce748d50e49453626ce3cea473ce9bdb24ad480b
Parinthapat Pengpun <parinzee@protonmail.com>
2022-06-19 14:25:06 +0700

    Added an info component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,9 @@ import Settings from "./components/Settings.jsx"
 
 function App() {
   return (
-    <div className="grid gap-32 h-screen w-screen grid-cols-1 md:grid-cols-5 bg-neutral-100 dark:bg-neutral-900">
-      <div className="grid grid-cols-1 grid-rows-4 col-span-1 md:col-span-3 content-center">
+    <div className="grid gap-32 h-screen w-screen grid-cols-1 md:grid-cols-5 bg-neutral-100 dark:bg-neutral-900 min-h-0 min-w-0">
+      <div className="grid grid-cols-1 grid-rows-1 col-span-1 md:col-span-3 content-center min-h-0 min-w-0">
         <Info />
-        <Settings />
       </div>
       <div className="col-span-1 md:col-span-2"></div>
     </div>
--- frontend/src/components/Info.jsx
+++ frontend/src/components/Info.jsx
@@ -1,6 +1,6 @@
 function Info() {
     return (
-        <div className="window font-sans m-3 text-black dark:text-white row-span-3">
+        <div className="shadow-xl shadow-blue-500/50 hover:shadow-indigo-500/50 window font-sans m-8 text-black dark:text-white row-span-1 overflow-auto max-h-max">
             <h1 className="text-3xl md:text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-blue-500">Automatic Thai Question Generation with MT5</h1>
             <h2 className="text-lg md:text-xl text-gray-700 dark:text-gray-400 mt-1">Created by: Parinthapat Pengpun</h2>
             <p className="text-lg md:text-xl dark:text-white text-black mt-5">This is an <b>mT5-small</b> model that has been finetuned to <b>generate questions</b> utilizing <b>only the context.</b> More info: GitHub, Blog.</p>
@@ -8,16 +8,16 @@ function Info() {
             <h2 className="text-2xl md:text-3xl text-black dark:text-white font-bold mt-1">Variants</h2>
             <ul className="text-lg md:text-xl mt-1 list-disc list-inside">
                 <li><span className="font-mono">aug:</span> Models with the dataset augmented (modified for better functionality)</li>
-                <li><span className="font-mono">sep:</span> Models with &quot;&lt;sep&gt;&quot; instead of &quot;1.&quot; for separation</li>
-                <li><span className="font-mono">numsep:</span> Models &quot;&lt;1&gt;&quot; instead of &quot;1.&quot; for separation</li>
+                <li><span className="font-mono">sep:</span> Generate questions with &quot;&lt;sep&gt;&quot; instead of &quot;1.&quot; for separation</li>
+                <li><span className="font-mono">numsep:</span> Generate questions with &quot;&lt;1&gt;&quot; instead of &quot;1.&quot; for separation</li>
             </ul>
             <hr className="mt-5" />
             <h2 className="text-2xl md:text-3xl text-black dark:text-white font-bold mt-1">Metrics</h2>
-            <table className="mt-1 table-fixed border-spacing-3 w-full border-separate border border-slate-500">
+            <table className="mt-3 table-fixed border-spacing-3 w-full border-separate border border-slate-500 overflow-scroll">
                 <thead>
                     <tr className="text-xl">
                         <th>Model</th>
-                        <th>METEOR</th>
+                        <th>Meteor</th>
                         <th>GLEU</th>
                         <th>BLEU</th>
                         <th>CHRF</th>
@@ -41,10 +41,18 @@ function Info() {
                         <td>0.46</td>
                         <td>0.86</td>
                     </tr>
+                    <tr>
+                        <td>numsep</td>
+                        <td>0.59</td>
+                        <td>0.36</td>
+                        <td>0.34</td>
+                        <td>0.46</td>
+                        <td>0.81</td>
+                    </tr>
                 </tbody>
             </table>
         </div>
     )
 }
 
-export default Info 
+export default Info
