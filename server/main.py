e127cf2cf30b6f948078812ae9d74d6cd8518adc
Parinthapat Pengpun <parinzee@protonmail.com>
2022-06-20 23:12:35 +0700

    Added CORS middleware for all origins

--- server/main.py
+++ server/main.py
@@ -4,7 +4,8 @@ import torch
 import psutil
 import uvicorn
 
-from fastapi import FastAPI 
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from transformers import MT5ForConditionalGeneration, MT5TokenizerFast
 
@@ -12,17 +13,25 @@ from transformers import MT5ForConditionalGeneration, MT5TokenizerFast
 print("Loading models...")
 models = {
     "default": {
-        "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg"),
-        "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg")
+        "model": MT5ForConditionalGeneration.from_pretrained(
+            "parinzee/mT5-small-thai-multiple-e2e-qg"
+        ),
+        "tokenizer": MT5TokenizerFast.from_pretrained(
+            f"parinzee/mT5-small-thai-multiple-e2e-qg"
+        ),
     },
     # "separated": {
     #     "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg-sep"),
     #     "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg-sep")
     # },
     "number_separated": {
-        "model": MT5ForConditionalGeneration.from_pretrained("parinzee/mT5-small-thai-multiple-e2e-qg-numsep"),
-        "tokenizer": MT5TokenizerFast.from_pretrained(f"parinzee/mT5-small-thai-multiple-e2e-qg-numsep")
-    } 
+        "model": MT5ForConditionalGeneration.from_pretrained(
+            "parinzee/mT5-small-thai-multiple-e2e-qg-numsep"
+        ),
+        "tokenizer": MT5TokenizerFast.from_pretrained(
+            f"parinzee/mT5-small-thai-multiple-e2e-qg-numsep"
+        ),
+    },
 }
 
 
@@ -38,20 +47,37 @@ class Args(BaseModel):
     top_k: int = 20
     num_return_sequences: int = 1
 
+
 print("Starting up app...")
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def health_check():
-    return {"models": list(models.keys()), "cpu_percent": psutil.cpu_percent(), "ram_percent": psutil.virtual_memory().percent} 
+    return {
+        "models": list(models.keys()),
+        "cpu_percent": psutil.cpu_percent(),
+        "ram_percent": psutil.virtual_memory().percent,
+    }
+
 
 @app.post("/")
 def model_endpoint(args: Args):
     model = models[args.model]["model"]
-    tokenizer= models[args.model]["tokenizer"]
+    tokenizer = models[args.model]["tokenizer"]
 
     with torch.no_grad():
-        input_ids = tokenizer.encode(args.input_text, return_tensors="pt", add_special_tokens=True)
+        input_ids = tokenizer.encode(
+            args.input_text, return_tensors="pt", add_special_tokens=True
+        )
         generated_ids = model.generate(
             input_ids=input_ids,
             num_beams=args.num_beams,
@@ -66,13 +92,13 @@ def model_endpoint(args: Args):
 
         preds = [
             tokenizer.decode(
-                g,
-                skip_special_tokens=True,
-                clean_up_tokenization_spaces=True
-            ) for g in generated_ids
+                g, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            for g in generated_ids
         ]
 
     return preds
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=80)
